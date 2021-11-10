a14710aa20f7c9a11fa9d24f7f8899bb50617ff3
St1ll3r <jorgempramos1999@gmail.com>
2021-11-10 13:00:31 +0000

    added calculate percentage and finished sumtwonumbers

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Main extends App {
   
   //----------
@@ -5,12 +7,46 @@ object Main extends App {
   //----------
 
   def returnGivenText(): String = { // Function reads a given string and prints it back
-    return ("You've said: " + (scala.io.StdIn.readLine("Say something\n")).toString)
+    "You've said: " + scala.io.StdIn.readLine("Say something\n")
   }
 
-/*   def sumTwoNumbers(num1 : String, num2 : String): String = { // Function reads two given integers, sums them and prints the result
-    return (num1 + num2).toString
-  } */
+  def sumTwoNumbers(num1 : Int, num2 : Int): Unit = { // Function reads two given integers, sums them and prints the result
+    println("The sum of the chosen numbers is: " + (num1 + num2).toString)
+  }
+
+  def calculatePercentageValueOfANumber(num : Int, percentage : Int): Unit = { // Function reads two given integers, sums them and prints the result
+    println("Answer: " + percentage + "% of " + num + " equals " + (num * (percentage/100.toDouble)).toInt)
+  }
+
+  @tailrec
+  def getPercentageInput(): Unit = { // Function reads two given integers, sums them and prints the result
+    try {
+      println("State a number to calculate percentage from")
+      val num = scala.io.StdIn.readInt()
+      println("State the percentage")
+      val percentage = scala.io.StdIn.readInt()
+      calculatePercentageValueOfANumber(num,percentage)
+    } catch {
+      case _: NumberFormatException =>
+        println("That's not a number, try again")
+        getPercentageInput()
+    }
+  }
+
+  @tailrec
+  def getTwoNumbersInput(): Unit = { // Function reads two given integers, sums them and prints the result
+    try {
+      println("State a number to sum")
+      val num1 = scala.io.StdIn.readInt()
+      println("State the second number")
+      val num2 = scala.io.StdIn.readInt()
+      sumTwoNumbers(num1,num2)
+    } catch {
+      case _: NumberFormatException =>
+        println("That's not a number, try again")
+        getTwoNumbersInput()
+    }
+  }
 
   //-----------------------
   // Validators
@@ -40,10 +76,11 @@ object Main extends App {
   // Menu and Functionality
   //-----------------------
 
+  @tailrec
   def getUserMenuInput(): Unit = { // Function reads the given option, validates it and sends it to the tests function
     try {
-      var answer = scala.io.StdIn.readInt()
-      tests(answer)
+        var answer = scala.io.StdIn.readInt()
+        tests(answer)
     } catch {
       case _: NumberFormatException =>
         println("That's not a number, try again")
@@ -52,29 +89,30 @@ object Main extends App {
   }
 
   def tests(option : Int): Unit = { option match { // Function meant to show the results of the functions
-      case 1 => { 
+      case 1 =>
         println(returnGivenText())
         init()
-      }
-      case 2 => { 
-        /* println(sumTwoNumbers("1","2")) */
+      case 2 =>
+        getTwoNumbersInput()
         init()
-      }
-      case 10 => { 
+      case 3 =>
+        getPercentageInput()
+        init()
+      case 10 =>
         println("Thanks for using the program")
-      }
-      case _ => { println("Please type a number from 1 to X") }
+      case _ =>  println("Please type a number from 1 to X")
     }
   }
 
-  def intro() = { // Function meant to show all the introductory text, such as the program name and the menu.
+  def intro(): Unit = { // Function meant to show all the introductory text, such as the program name and the menu.
       println("Welcome to my Scala Playground. This is where I've done my first few functions to get started with Scala.\n\n" +
         "1 - Return what you've just typed\n" +
-        "2 - Sum two numbers\n" + 
+        "2 - Sum two numbers\n" +
+        "3 - Calculate given percentage of a given number\n" +
         "10 - Quit")
   }
 
-  def init() = { // Function responsible for initiating the program
+  def init(): Unit = { // Function responsible for initiating the program
     intro()
     getUserMenuInput()
   }
